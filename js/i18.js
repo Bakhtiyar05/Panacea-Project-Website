9c58b0c50ecc575808feed531369f9e5aa82afaf
Bakhtiyar Balabayli <bakhtiyarbli04@gmail.com>
2026-03-17 11:37:10 +0400

    11.37

--- js/i18.js
+++ js/i18.js
@@ -1,24 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     var arrLang = {
+
         'az': {
+
+            /* Navbar */
+
             'nav_Home': 'Əsas səhifə',
             'nav_About': 'Haqqımızda',
             'nav_Services': 'Xidmətlər',
+            'nav_Partners': 'Partnyorlar',
             'nav_Contact': 'Əlaqə',
+        
+            /*  */
         },
+
         'en': {
+
+            /* Navbar */
+
             'nav_Home': 'Home',
             'nav_About': 'About Us',
             'nav_Services': 'Services',
+            'nav_Partners': 'Partners',
             'nav_Contact': 'Contact',
+        
+            /*  */
         },
+
         'ru': {
+
+            /* Navbar */
+
             'nav_Home': 'Главная',
             'nav_About': 'О нас',
             'nav_Services': 'Услуги',
+            'nav_Partners': 'Партнеры',
             'nav_Contact': 'Контакт',
-        }
+        
+            /*  */
+        },
     };
 
 
